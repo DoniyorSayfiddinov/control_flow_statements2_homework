f68a1a483cf81ee5c85b99ddf2ff5cf955fdbf93
DoniyorSayfiddinov <sayfiddinovdoniyor833@gmail.com>
2023-10-03 09:06:07 +0500

    3-ish-di

--- if03.py
+++ if03.py
@@ -8,4 +8,10 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if (a-c>0 or c-a>0)and (a>b>c or a<b<c):
+        return b 
+    if (b-c>0 or c-b>0)and (b>a>c or b<a<c):
+        return a 
+    else :
+        return c 
+print(main(25,89,23))
